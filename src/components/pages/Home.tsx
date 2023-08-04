6b13d6a5bbdd146f3fdedbf9525a465c70febfec
Saoa35 <saoamah@gmail.com>
2023-08-04 11:52:28 +0400

    * home

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,15 +1,55 @@
-import React, {useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {View, StyleSheet, TextInput} from 'react-native';
-
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  Button,
+  Modal,
+} from 'react-native';
+import {ContactBlock} from '../ContactBlock';
+import {IContact} from '../../@types';
+import {filter} from '../../helpers/filter';
+import Loader from '../Loader';
+import ModalAddNew from '../ModalAddNew';
+import {DataContext} from '../../context/dataContext';
 type RootStackParamList = {
   Home: undefined;
+  Contact: {contact: IContact};
 };
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
-const Home: React.FC<Props> = () => {
+const Home: React.FC<Props> = ({navigation}) => {
+  const {data, loading, addContact} = useContext(DataContext);
+  const [dataToShow, setDataToShow] = useState<IContact[]>(data);
   const [search, setSearch] = useState('');
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModalClose = () => setShowModal(false);
+  const handleModalOpen = () => setShowModal(true);
+
+  useEffect(() => {
+    setDataToShow(filter(data, search));
+  }, [data, search]);
+
+  const handleClick = (contact: IContact) => {
+    navigation.navigate('Contact', {contact});
+  };
+
+  const handleAdd = (body: IContact, error?: boolean) => {
+    if (error) {
+      return;
+    }
+    addContact(body);
+    handleModalClose();
+    navigation.navigate('Contact', {contact: body});
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
 
   return (
     <View style={styles.container}>
@@ -19,6 +59,24 @@ const Home: React.FC<Props> = () => {
         value={search}
         onChangeText={setSearch}
       />
+      <Button title="Add new contact" onPress={handleModalOpen} />
+
+      <Modal
+        animationType="slide"
+        visible={showModal}
+        onRequestClose={handleModalClose}>
+        <ModalAddNew onSubmit={handleAdd} />
+      </Modal>
+
+      <ScrollView contentContainerStyle={styles.contactBlock}>
+        {dataToShow.map(contact => (
+          <ContactBlock
+            key={contact.id}
+            {...contact}
+            onClick={() => handleClick(contact)}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 };
